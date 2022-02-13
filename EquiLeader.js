cf83ce55e6fb036703c9447a4215c8b6fc4b1a42
cansutuzmen <52562509+cansutuzmen@users.noreply.github.com>
2022-02-13 12:02:39 +0300

    Create EquiLeader.js

---
+++ EquiLeader.js
@@ -0,0 +1,46 @@
+//Codility - EquiLeader Problem
+/*
+https://app.codility.com/programmers/lessons/8-leader/equi_leader/
+*/
+
+
+function solution(A) {
+    if(A.length === 1){
+        return 0;
+    }
+
+    let map = {};
+    let max = -1;
+    let leader = null;
+
+    for(let i = 0; i < A.length; i++){
+        if(A[i] in map){
+            map[A[i]]++;
+            if(map[A[i]] > Math.floor(A.length / 2)){
+                max = map[A[i]];
+                leader = A[i];
+            }
+        }else{
+            map[A[i]] = 1;
+        }
+    }
+
+    if(max === -1){
+        return 0;
+    }
+
+    let leaderCount = 0;
+    let equiLeader = 0;
+
+    for(let i = 0; i < A.length -1; i++){
+        if((leaderCount > Math.floor(i / 2)) && (max - leaderCount > Math.floor((A.length - i) / 2))){
+            equiLeader++;
+        }
+
+        if(A[i] === leader){
+            leaderCount++;
+        }
+    }
+
+    return equiLeader;
+}
